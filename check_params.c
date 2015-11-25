287df5dc69b802245920b43b5d4530e07c851deb
Philippe SAINT-JACQUES <psaint-j@e3r11p6.42.fr>
2015-11-25 18:01:18 +0100

    all done

--- check_params.c
+++ check_params.c
@@ -6,13 +6,13 @@
 /*   By: psaint-j <psaint-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/06 17:05:49 by psaint-j          #+#    #+#             */
-/*   Updated: 2015/11/25 17:13:30 by aaudiber         ###   ########.fr       */
+/*   Updated: 2015/11/25 18:01:01 by psaint-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void		prm_new(t_prm * s)
+void		prm_new(t_prm *s)
 {
 	s->error = (char **)malloc(sizeof(char*) * (s->e + 1));
 	s->file = (char **)malloc(sizeof(char*) * (s->f + 1));
@@ -20,7 +20,6 @@ void		prm_new(t_prm * s)
 	s->f--;
 	s->e--;
 	s->d--;
-	return (s);
 }
 
 t_prm		*check_params(char **paths, t_prm *s, int i)
@@ -30,24 +29,24 @@ t_prm		*check_params(char **paths, t_prm *s, int i)
 	struct stat		t;
 
 	prm_new(s);
-	while (av[i])
+	while (paths[i])
 	{
-		dir = opendir(av[i]);
-		v = stat(av[i], &t);
+		dir = opendir(paths[i]);
+		v = stat(paths[i], &t);
 		if (dir)
 		{
-			s->ddir[s->d] = av[i];
+			s->ddir[s->d] = paths[i];
 			s->d--;
 			closedir(dir);
 		}
 		else if (v != 0)
 		{
-			s->error[s->e] = av[i];
+			s->error[s->e] = paths[i];
 			s->e--;
 		}
 		else if ((t.st_mode & S_IFREG))
 		{
-			s->file[s->f] = av[i];
+			s->file[s->f] = paths[i];
 			s->f--;
 		}
 		i++;
