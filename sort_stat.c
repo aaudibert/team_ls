5f6561d0740ee53f9aeb98f86f5e441d5d561185
migoomig42 <jumiguel@e2r7p12.42.fr>
2015-12-04 18:34:55 +0100

    date

--- sort_stat.c
+++ sort_stat.c
@@ -6,28 +6,20 @@
 /*   By: jumiguel <jumiguel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 17:47:32 by jumiguel          #+#    #+#             */
-/*   Updated: 2015/12/04 18:23:32 by jumiguel         ###   ########.fr       */
+/*   Updated: 2015/12/04 18:34:27 by jumiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	sort_dir(t_file *dir)
-{
-	char	*tmp;
-	int		i;
-
-	while (dir->next)
-}
-
-void	sort_dir(t_file *dir)
+void	sort_date(t_file *dir)
 {
 	char	*tmp;
 	int		i;
 
 	while (dir->next)
 	{
-		if (ft_strcmp(dir->f_name, dir->next->f_name) > 0)
+		if (dir->date < dir->next->date)
 		{
 			tmp = dir->f_name;
 			i = dir->izdir;
